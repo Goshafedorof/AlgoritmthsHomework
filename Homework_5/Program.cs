1eb96f7082de87ed1a0d3b843c897e8e24d7fd0a
g0shafedorof@yandex.ru <g0shafedorof@yandex.ru>
2020-04-02 17:51:05 +0300

    Приступил к ДЗ №5

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -10,6 +10,35 @@ namespace Homework_5
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                #region Menu
+                Console.WriteLine("Выполнил: Федоров Игорь.");
+                Console.WriteLine("Меню:");
+                Console.WriteLine("# - Краткое описание программы");
+                Console.WriteLine("1 - Перевод из десятичной в двоичную систему счисления;");
+
+                Console.Write("\nВведите номер программы (для выхода - любой другой символ): ");
+                #endregion
+
+                string sign = Console.ReadLine();
+
+                Console.Clear();
+
+                switch (sign)
+                {
+                    case "1":
+                        
+                        break;
+                    default:
+                        return;
+                }
+
+                Console.WriteLine("Для перехода в меню нажмите любую клавишу.");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
         }
     }
 }
